249fb5c93ed37cccb28ed58f6daed33a0a8e397a
Jiwoo Ahn <ilp_rincipe@naver.com>
2022-12-21 22:10:47 +0900

    modifying try-catch scope

--- 05/ex00/main.cpp
+++ 05/ex00/main.cpp
@@ -2,17 +2,41 @@
 
 int main()
 {
-  Bureaucrat a(5);
-
-  Bureaucrat b(0);
-  Bureaucrat c(1000);
-
-  std::cout << a << std::endl;
-
-  for (int i = 0; i < 10; i++)
+  try
   {
-      a.upgradeBureaucrat();
-      std::cout << a << std::endl;
+    std::cout << "\n=CREATE BUREAUCRAT A=\n" << std::endl;
+    Bureaucrat a(5);
+    std::cout << a << std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+        a.upgradeBureaucrat();
+        std::cout << a << std::endl;
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  try
+  {
+    std::cout << "\n=CREATE BUREAUCRAT B=\n" << std::endl;
+    Bureaucrat b(0);
+    std::cout << b << std::endl;
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  try
+  {
+    std::cout << "\n=CREATE BUREAUCRAT C=\n" << std::endl;
+    Bureaucrat c(1000);
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
   }
 
   return (0);
