e4661ece7a6b0bd4259494de880e26a3de01bc3c
Jiwoo Ahn <ilp_rincipe@naver.com>
2022-12-21 22:17:10 +0900

    modifying try-catch scope in 01

--- 05/ex01/main.cpp
+++ 05/ex01/main.cpp
@@ -2,18 +2,25 @@
 
 int main()
 {
-  Form A;
-  std::cout << A << std::endl;
+  try
+  {
+    Form A;
+    std::cout << A << std::endl;
 
-  Bureaucrat B(101);
-  std::cout << B << std::endl;
+    Bureaucrat B(101);
+    std::cout << B << std::endl;
 
-  A.beSigned(B);
+    A.beSigned(B);
 
-  B.upgradeBureaucrat();
-  A.beSigned(B);
+    B.upgradeBureaucrat();
+    A.beSigned(B);
 
-  std::cout << A << std::endl;
+    std::cout << A << std::endl;
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
 
   return (0);
 }
