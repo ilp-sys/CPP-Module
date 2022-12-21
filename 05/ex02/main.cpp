c45f1c7e7d033abecdbd0ab4059d3300920635b6
Jiwoo Ahn <ilp_rincipe@naver.com>
2022-12-21 22:26:13 +0900

    modifying try-catch scope in 02

--- 05/ex02/main.cpp
+++ 05/ex02/main.cpp
@@ -7,32 +7,38 @@
 
 int main()
 {
-  Bureaucrat a(1);
-  Bureaucrat b(150);
+  try
+  {
+    Bureaucrat a(1);
+    Bureaucrat b(150);
 
-  std::cout << "\nTest for ShrubberyCreationForm" << std::endl;
-  ShrubberyCreationForm sForm("home");
+    std::cout << "\n=Test for ShrubberyCreationForm=" << std::endl;
+    ShrubberyCreationForm sForm("home");
 
-  a.signForm(sForm);
-  a.executeForm(sForm);
+    a.signForm(sForm);
+    a.executeForm(sForm);
 
-  std::cout << "\nTest for RobotomyRequestForm" << std::endl;
-  RobotomyRequestForm rForm("robo");
-  a.signForm(rForm);
-  for (int i = 0; i < 5; i++)
-  {  
-    a.executeForm(rForm);
-    sleep(1);
-    std::cout << std::endl;
+    std::cout << "\n=Test for RobotomyRequestForm=" << std::endl;
+    RobotomyRequestForm rForm("robo");
+    a.signForm(rForm);
+    for (int i = 0; i < 5; i++)
+    {  
+      a.executeForm(rForm);
+      sleep(1);
+      std::cout << std::endl;
+    }
+    
+    std::cout << "\n=Test for PresidentialPardonForm=" << std::endl;
+    PresidentialPardonForm pForm("42");
+    b.signForm(pForm);
+    a.signForm(pForm);
+    b.executeForm(pForm);
+    a.executeForm(pForm);
   }
-  
-  //Test for PresidentialPardonForm
-  std::cout << "\nTest for PresidentialPardonForm" << std::endl;
-  PresidentialPardonForm pForm("42");
-  b.signForm(pForm);
-  a.signForm(pForm);
-  b.executeForm(pForm);
-  a.executeForm(pForm);
-  
+  catch (std::exception &e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
   return (0);
 }
